5281986ed9e2332077be9662a627ae64e048720e
이유리 <yuri@iyuliui-MacBookPro.local>
2019-10-11 02:02:33 +0900

    mod page name

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Search from '../pages/Search/Search';
 import WordList from '../pages/WordList/WordList';
-import WordDetail from '../pages/WordDetail/WordList';
-import Fiiltering from '../pages/Fiiltering/WordList';
+import Home from '../pages/Home/Home';
+import WordDetail from '../pages/WordDetail/WordDetail';
 
 const Root: React.FC = () => (
     <BrowserRouter>
         <Switch>
-            <Route path="/" exact component={Search} />
-            <Route path="/filter" component={Fiiltering} />
+            <Route path="/" exact component={Home} />
+            <Route path="/Search" component={Search} />
             <Route path="/list" component={WordList} />
             <Route path="/detail" component={WordDetail} />
             <Redirect path="*" to="/" />
