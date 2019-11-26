4fdc913740b01db253661ec52e0c00b29bff28bb
yurilee9696 <yurilee9696@gmail.com>
2019-11-27 02:09:24 +0900

    add atomic icon component 추가

---
+++ src/components/icon/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+export interface ButtonProps {
+    children: React.ReactNode,
+    size: 'small' | 'medium' | 'large',
+}
+
+const Icon = (props: ButtonProps): React.ReactElement<ButtonProps> => {
+    const { children, size } = props
+    const StyleSpan = styled.span`
+        svg {
+            width: 20px;
+            height: 20px;
+            mask-size: contain;
+            mask-position: 50% 50%;
+            mask-repeat: no-repeat; 
+
+            ${size == 'small' && css`
+                width: 12px;
+                height: 12px;
+            `}
+
+            ${size == 'large' && css`
+                width: 28px;
+                height: 28px;
+            `}
+        }
+    `
+
+    return (
+        <StyleSpan>{children}</StyleSpan>
+    )
+}
+
+Icon.defaultProps = {
+    size: 'medium'
+}
+
+export default Icon
