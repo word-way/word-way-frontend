ef9086c2a11281805b3a6b3b960487e25a35a4b6
YuRi Lee <yurilee9696@gmail.com>
2019-11-29 23:08:57 +0900

    button component 수정 및 icon component 추가 (#13)
    * mod package (동적 style 수정을 위한 styled-components pacakge 추가)
    
    * mod atomic button 컴포넌트 수정
    
    * add atomic icon component 추가
    
    * add icon component에서 사용할 수 있는 svg들 추가
    
    * add icon storybook

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,40 +1,66 @@
 import React from 'react';
 import classnames from 'classnames';
 import styles from './button.module.scss';
-
-export enum ButtonType {
-  BUTTON = 'button',
-  RESET = 'reset',
-  SUBMIT = 'submit',
-}
-
-export const ButtonTheme = {
-  DEFAULT: 'default',
-  ROUNDED: 'rounded',
-}
-
-export const ButtonSize = {
-  SMALL: 'small',
-  MEDIUM: 'medium',
-  LARGE: 'large',
-}
+import styled, { css } from 'styled-components';
+import { ColorList, TextColor } from '../../static/const';
 
 export interface ButtonProps {
-  type: ButtonType,
-  theme: string,
-  size: string,
+  type: 'button' | 'reset' | 'submit',
+  theme: 'default' | 'rounded',
+  size: 'small' | 'medium' | 'large',
+  variant: 'text' | 'outlined' | 'contained',
   onClick: () => void,
   children: React.ReactNode,
   className: string,
   disabled: boolean,
+  color: string
 }
 
 const Button = (props: ButtonProps): React.ReactElement<ButtonProps> => {
-  const { type, onClick, children, theme, size, className, disabled } = props
+  const { type, onClick, children, theme, size, className, disabled, variant, color } = props;
+
+  const themeColor = color ? Object(ColorList)[color] : ColorList.default;
+  const StyleButton = styled.button`
+    background: transparent;
+    border-radius: 4px;
+    color: ${TextColor.darkgray1};
+    padding: 6px 16px;
+    min-width: 64px;
+    letter-spacing: -0.9px;
+    font-weight: 500;
+    font-size: 1.1em;
+
+    ${variant === 'text' && css`
+    border: 0;
+    `}
+
+    ${variant === 'contained' && css`
+      color: ${TextColor.gray1};
+      background: ${themeColor};
+      border: 1px solid ${themeColor};
+    `}
+
+    ${variant === 'outlined' && css`
+    color: ${themeColor};
+    border: 1px solid ${themeColor};
+    `}
+
+    ${theme === 'rounded' && css`
+    border-radius: 20px;
+    min-width: 90px;
+    `}
+
+    ${variant === 'contained' && color === 'default' && css`
+      color: ${TextColor.darkgray1};
+      background: ${themeColor};
+      border: 1px solid ${themeColor};
+    `}
+
+    ${size === 'small' && css`
+    font-size: 0.85rem;
+    `}
+  `
   const classProps: string = classnames(
-    styles.button,
-    styles[theme],
-    styles[size],
     {
       [styles.disabled]: disabled,
     },
@@ -42,19 +68,21 @@ const Button = (props: ButtonProps): React.ReactElement<ButtonProps> => {
   )
 
   return (
-    <button type={type} onClick={onClick} disabled={disabled} className={classProps}>
+    <StyleButton type={type} onClick={onClick} disabled={disabled} className={classProps}>
       {children}
-    </button>
+    </StyleButton>
   )
 }
 
 Button.defaultProps = {
-  type: ButtonType.BUTTON,
-  theme: ButtonTheme.DEFAULT,
-  size: ButtonSize.MEDIUM,
-  onClick: () => {},
+  type: 'button',
+  theme: 'default',
+  size: 'medium',
+  onClick: () => { },
   className: null,
   disabled: false,
+  variant: 'contain',
+  color: 'primary'
 }
 
 export default Button
