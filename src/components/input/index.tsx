3291f3ee7dd861eaeca6e718b77d7f3f38554616
yool <yool@spoqa.com>
2020-01-19 01:23:23 +0900

    Create Input Custom Component

---
+++ src/components/input/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import classnames from 'classnames';
+import styled from 'styled-components';
+import styles from './input.module.scss';
+import { css } from 'styled-components';
+
+export interface InputProps {
+  autoFocus: boolean;
+  disabled: boolean;
+  value: string;
+  label: string;
+  color: string;
+  type: 'text' | 'number' | 'password';
+  className: string;
+  placeholder?: string;
+  onClick?: () => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const BaseInput = (props: InputProps): React.ReactElement<InputProps> => {
+  const { 
+    disabled,
+    className,
+    color,
+    ...inputProps
+  } = props;
+
+  const classProps: string = classnames(
+    {
+      [styles.disabled]: disabled,
+    },
+    className,
+  );
+  return (
+    <input
+      className={classProps} 
+      {...inputProps}
+    />
+  );
+};
+
+const Input = styled(BaseInput)(() => {
+  return css`
+    background: transparent;
+    border-radius: 4px;
+    padding: 6px 16px;
+    min-width: 64px;
+    letter-spacing: -0.9px;
+    font-weight: 500;
+    font-size: 1.1em;
+    border: 1px solid black;
+  `;
+});
+
+BaseInput.defaultProps = {
+  autofocus: false,
+  disabled: false,
+  color: 'primary',
+  type: 'text',
+  label: '',
+  className: null,
+  value: '',
+};
+
+export default Input;
