90417f9c5d5d86c9354ea5c765b772a0d5c7d7fc
yurilee9696 <yurilee9696@gmail.com>
2020-02-12 04:09:06 +0900

    add card component 추가

---
+++ src/components/molecules/card/card.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { withKnobs, boolean, object } from '@storybook/addon-knobs';
+import { ThemeDecorator } from '../../../static/themeDecorator';
+import Card from './index';
+
+export default {
+    title: 'components|atomic/Card',
+    component: Card,
+    decorators: [withKnobs, ThemeDecorator],
+};
+
+const cardValue = {
+    title: '지향하다',
+    desc: '[동사] 어떤 목표로 뜻이 쏠리어 향하다.',
+    words: [
+        {
+            title: '향하다',
+        },
+        {
+            title: '쏠리다',
+        },
+        {
+            title: '쏠리다',
+        },
+        {
+            title: '쏠리다',
+        },
+    ],
+};
+const multiCardValue = {
+    title: '떼다',
+    desc: '붙어 있거나 잇닿은 것을 떨어지게 하다.',
+    words: [],
+};
+
+export const card = () => {
+    const canDel = boolean('canDel', true);
+    const data = object('data', cardValue, 'card data');
+    return <Card canDel={canDel} data={data}></Card>;
+};
+card.story = {
+    name: 'Default',
+};
+
+export const multiCard = () => {
+    const canDel = boolean('canDel', true);
+    const data = object('data', multiCardValue, 'card data');
+    return <Card canDel={canDel} data={data}></Card>;
+};
---
+++ src/components/molecules/card/index.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { ReactComponent as CloseIc } from '../../../assets/icons/cross.svg';
+import { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
+import Label from '../../label';
+import Button from '../../button';
+import IconButton from '../../iconButton';
+
+export interface Word {
+    title: string;
+}
+export interface CardModel {
+    title: string;
+    desc: string;
+    words: Word[];
+}
+export interface CardProps {
+    canDel: boolean;
+    data: CardModel;
+}
+
+const Card = (props: CardProps): React.ReactElement<CardProps> => {
+    const {
+        canDel,
+        data,
+    } = props;
+    const multi = data.words.length > 0 ? false : true;
+    const theme = useContext(ThemeContext);
+
+    const CardIc = styled(IconButton)`
+        position: absolute;
+        right: 4px;
+        top: 4px;
+    `;
+    const StyleDiv = styled.div`
+        width: 320px;
+        border-radius:20px;
+        background: white;
+        border-radius: 8px;
+        box-shadow: 0px 0px 16px 0px rgba(84, 84, 84, .1);
+        position: relative;
+        min-height: 135px;
+        .contentWrapp {
+            padding: 18px 18px 0px
+        }
+        ${multi === true && css`
+            display: flex;
+            align-items: center;
+            &::after {
+                width:95%;
+                height:100%;
+                content: '';
+                position: absolute;
+                left: calc((100% - 95%) / 2);
+                top: 10px;
+                z-index: -1;
+                background: white;
+                border-radius: 8px;
+                box-shadow: 0px 0px 16px 0px rgba(84, 84, 84, .1);
+            }
+        `}
+    `;
+    const DescLabel = styled(Label)`
+        padding: 8px 0px 16px 0px;
+        white-space: nowrap;
+    `;
+    const KeywordWrapp = styled.div`
+        position: relative;
+        white-space: nowrap;
+        overflow-x: scroll;
+        -ms-overflow-style: none;
+        padding-bottom: 18px;
+        &::-webkit-scrollbar {
+            display: none !important;
+        }
+        * {
+            margin: 0px 4px;
+            &:first-of-type {
+                margin-left: 18px;
+            }
+            &:last-of-type {
+                margin-right: 18px;
+            }
+        }
+    `;
+
+    return (
+        <StyleDiv>
+            <div className='contentWrapp'>
+                {canDel && <CardIc icon={<CloseIc />} variant='text' size='small'></CardIc>}
+                <Label color={theme.colors.blue}
+                    weight='light'
+                    size='large'>{data.title}</Label>
+                <DescLabel color={theme.colors.g500}
+                    weight='light'
+                    size='13px'>{multi ? ('2개의 뜻 존재') : data.desc}
+                </DescLabel>
+            </div>
+            {!multi &&
+                <KeywordWrapp>
+                    {data.words.map((word: Word, index: number) =>
+                        <Button color={theme.colors.g100} corner='rounded' variant='contained' size='small' key={index}>
+                            {word.title}
+                        </Button>
+                    )}
+                </KeywordWrapp>
+            }
+        </StyleDiv>
+    );
+};
+
+Card.defaultProps = {
+    canDel: true,
+};
+
+export default Card;
