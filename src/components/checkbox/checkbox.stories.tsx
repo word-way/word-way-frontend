e09707bb0131c72837764fdcc5b9b3bdb3f53fd3
yurilee9696 <yurilee9696@gmail.com>
2020-03-26 06:57:58 +0900

    ADD checkbox component 추가

---
+++ src/components/checkbox/checkbox.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ThemeDecorator } from '../../static/themeDecorator';
+import Checkbox from './index';
+
+export default {
+    title: 'components|Atoms/Checkbox',
+    component: Checkbox,
+    decorators: [ThemeDecorator],
+};
+
+export const checkbox = () => {
+    return <Checkbox></Checkbox>;
+};
+checkbox.story = {
+    name: 'Default',
+};
