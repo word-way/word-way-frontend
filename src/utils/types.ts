92e5dc63447bc415057e42e752eedf7846312415
yool <yool@spoqa.com>
2020-04-30 22:16:55 +0900

    Make word and pronunciation model type

---
+++ src/utils/types.ts
@@ -0,0 +1,24 @@
+export type WordPart =
+  'verb'
+  | 'noun'
+  | 'pronoun'
+  | 'numeral'
+  | 'postposition'
+  | 'adjective'
+  | 'adverb'
+  | 'interjection'
+  | 'unknown';
+
+export type WordModel = {
+  id: string;
+  contents: string;
+  part: WordPart;
+  relatedPronunciations?: string[];
+}
+
+export type CardModel = {
+  id: string;
+  pronunciation: string;
+  words: WordModel[];
+  relatedPronunciations?: string[];
+};
