3eb9ed14c1ff2a028f7d0f128ae41e953abcd9a5
yool <yool@spoqa.com>
2020-04-30 19:17:08 +0900

    Change type that manage query string

--- src/pages/WordListPage.tsx
+++ src/pages/WordListPage.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { ArrayParam, useQueryParams } from 'use-query-params';
 
 interface WordListProps {
   searchWord: string;
 }
 
 const WordListPage: React.FC<RouteComponentProps<WordListProps>> = (props) => {
-  function gotoDetail() {
+  const [query, _] = useQueryParams({
+    words: ArrayParam,
+  });
+
+  const gotoDetail = () => {
     const url = '/detail';
     props.history.push(url);
-  }
-  const { searchWord } = props.match.params;
+  };
+
   return (
     <div>
       WordList Page
       <div>
-        <h2>단어 -> {searchWord}</h2>
+        <h2>단어 -> {query.words}</h2>
       </div>
       <div onClick={gotoDetail}>리스트 1</div>
       <div>리스트 2</div>
