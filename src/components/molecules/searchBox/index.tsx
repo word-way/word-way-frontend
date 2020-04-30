17bd506139e5fbf83a284ee729de93a6a64d18b5
yool <yool@spoqa.com>
2020-04-30 19:29:07 +0900

    Make a SearchBox component

---
+++ src/components/molecules/searchBox/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+import Button from '../../button';
+
+interface SearchBoxProp {
+  onSearch: (word: string) => void,
+}
+
+const SearchBox = (props: SearchBoxProp) => {
+  const { onSearch } = props;
+  const [word, setWord] = useState<string>('');
+
+  const changeSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWord(event.target.value)
+  }
+
+  return (
+    <form>
+      <input onChange={changeSearchInput} defaultValue={word}/>
+      <Button onClick={() => onSearch(word)}>검색</Button>
+    </form>
+  );
+};
+
+export default SearchBox;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { RouteComponentProps } from 'react-router';
 
-import Button from '../components/button';
+import SearchBox from '../components/molecules/searchBox';
 
 const HomePage: React.FC<RouteComponentProps> = (props) => {
-  const [word, setWord] = useState<string>();
-
-  const searchWord = () => {
+  const searchWord = (word: string) => {
     props.history.push(`/search?words=${word}`);
   }
 
-  const changeSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setWord(event.target.value)
-  }
-
   return (
-    <>
-      <form>
-        <input onChange={changeSearchInput} defaultValue={word}/>
-        <Button onClick={searchWord}>검색</Button>
-      </form>
-    </>
+    <SearchBox onSearch={searchWord} />
   );
 };
 
