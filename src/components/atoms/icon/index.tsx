0e3d5c6bd2a09f813b10298ff2b129e1da7f9c75
yurilee9696 <yurilee9696@gmail.com>
2020-06-06 15:38:13 +0900

    Style: atoms component style 밖으로 분리

--- src/components/atoms/icon/index.tsx
+++ src/components/atoms/icon/index.tsx
@@ -1,41 +1,47 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export interface IconProps {
-    children: React.ReactNode;
-    size: 'small' | 'medium' | 'large';
-    className?: string;
-    color: string;
+  children: React.ReactNode;
+  size: 'small' | 'medium' | 'large';
+  className?: string;
+  color: string;
 }
 
 const Icon = (props: IconProps): React.ReactElement<IconProps> => {
-    const {
-        children,
-        size,
-        className,
-        color,
-    } = props;
-    const fontSize = (!size || size === 'medium') ? 1.1 : (size === 'small' ? 0.8 : 1.8);
-    const StyleSpan = styled.span`
-        width: 1em;
-        height: 100%;
-        font-size: ${fontSize + 'em'};
-        display: flex;
-        svg {
-            width: 100%;
-            height: 100%;
-            fill: ${color};
-        }
-    `;
+  const {
+    children,
+    className,
+  } = props;
 
-    return (
-        <StyleSpan className={className}>{children}</StyleSpan>
-    );
+  return (
+    <span className={className}>{children}</span>
+  );
 };
+
+const StyledIcon = styled(Icon)((props) => {
+  const {
+    size,
+    color,
+  } = props;
+  const fontSize = (!size || size === 'medium') ? 1.1 : (size === 'small' ? 0.8 : 1.8);
+
+  return css`
+    width: 1em;
+    height: 100%;
+    font-size: ${fontSize + 'em'};
+    display: flex;
+    svg {
+      width: 100%;
+      height: 100%;
+      fill: ${color};
+    }
+  `;
+});
 
 Icon.defaultProps = {
-    size: 'medium',
-    color: 'black',
+  size: 'medium',
+  color: 'black',
 };
 
-export default Icon;
+export default StyledIcon;
--- src/components/atoms/label/index.tsx
+++ src/components/atoms/label/index.tsx
@@ -2,63 +2,69 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 export interface LabelProps {
-    children: React.ReactNode;
-    color: string;
-    size: 'medium' | 'large' | string;
-    weight: 'light' | 'normal' | 'semi' | 'bold';
-    className?: string;
+  children: React.ReactNode;
+  color: string;
+  size: 'medium' | 'large' | string;
+  weight: 'light' | 'normal' | 'semi' | 'bold';
+  className?: string;
 }
 
 const Label = (props: LabelProps): React.ReactElement<LabelProps> => {
-    const {
-        children,
-        size,
-        color,
-        weight,
-        className,
-    } = props;
+  const {
+    children,
+    className,
+  } = props;
 
-    let fontSize;
-    switch (size) {
-        case 'large':
-            fontSize = 24;
-            break;
-        case 'medium':
-            fontSize = 15;
-            break;
-        case 'small':
-            fontSize = 11;
-            break;
-    }
-    fontSize = fontSize ? `${fontSize}px` : size;
-    const StyleDiv = styled.div`
-        font-weight: normal;
-        color: ${color};
-        padding: calc(${fontSize} * 0.1);
-        font-size: ${fontSize};
-        ${size === 'medium' && css`
-            font-weight: 500;
-        `}
-        ${weight === 'light' && css`
-            font-weight: 300;
-        `}
-        ${weight === 'semi' && css`
-            font-weight: 500;
-        `}
-        ${weight === 'bold' && css`
-            font-weight: bold;
-        `}
-    `;
-
-    return (
-        <StyleDiv className={className}>{children}</StyleDiv>
-    );
+  return (
+    <div className={className}>{children}</div>
+  );
 };
+
+const StyledLabel = styled(Label)((props) => {
+  const {
+    size,
+    color,
+    weight,
+  } = props;
+
+  let fontSize;
+  switch (size) {
+    case 'large':
+      fontSize = 24;
+      break;
+    case 'medium':
+      fontSize = 15;
+      break;
+    case 'small':
+      fontSize = 11;
+      break;
+  }
+  fontSize = fontSize ? `${fontSize}px` : size;
+
+  return css`
+    font-weight: normal;
+    color: ${color};
+    padding: calc(${fontSize} * 0.1);
+    font-size: ${fontSize};
+    ${size === 'medium' && css`
+        font-weight: 500;
+    `}
+    ${weight === 'light' && css`
+        font-weight: 300;
+    `}
+    ${weight === 'semi' && css`
+        font-weight: 500;
+    `}
+    ${weight === 'bold' && css`
+        font-weight: bold;
+    `}
+  `;
+});
 
 Label.defaultProps = {
-    size: 'medium',
-    color: 'black',
-    weight: 'normal',
+  size: 'medium',
+  color: 'black',
+  weight: 'normal',
 };
 
-export default Label;
+export default StyledLabel;
